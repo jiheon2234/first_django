5d5138216fbfc5d2005cb3ead9082684dc6e90ac
jiheon Kim <jiheon2234@gmail.com>
2022-05-27 13:28:12 +0900

    DB관리하기

--- sales/models.py
+++ sales/models.py
@@ -23,6 +23,8 @@ class Sale(models.Model):
 class Person(models.Model):
     회원 = models.OneToOneField(아이디, on_delete=models.CASCADE) #1대1
 
+    def __str__(self) -> str:
+        return self.회원.email
 
     #python manage.py migrate
 
