fc20c32cadd6c79326e0ca8ff92d91350ceeb97b
jiheon Kim <jiheon2234@gmail.com>
2022-05-27 16:49:19 +0900

    htmlcontext

--- sales/views.py
+++ sales/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render     #render=랜더링생각
 from django.http import HttpResponse
-
+from .models import 아이디
 # Create your views here.
 
 def 홈페이지(request):
-
-    return render(request, "아무거나2.html") #기본적으로 templates밑으로 바꿀러면 settings.py설정
+    드실분 = 아이디.objects.all()
+    context = {
+        "메뉴명" : "짜장",
+        "가격" : "700원",
+        "손님" : 드실분,
+    }
+    return render(request, "아무거나2.html", context) #context=html을 동적으로만드는 dict()
+    #기본적으로 templates밑으로 바꿀러면 settings.py설정
