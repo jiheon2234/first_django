e514ebba92826117fe703541b3c4860356f3bf6e
jiheon Kim <jiheon2234@gmail.com>
2022-05-27 12:49:30 +0900

    원래있는db형식 가져오기

--- sales/models.py
+++ sales/models.py
@@ -1,18 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser #이미있는 모델 가져오기 (회원관리)
 
 # Create your models here. (DB)
 
+class 아이디(AbstractUser): #상속 
+    pass
+
 class Sale(models.Model):
-    경로_선택 = (
-        ('nr','네이버'),
-        ('shop2','샵투원드'),
-        ('news','이메일뉴스레터')
-    )
+    
     first_name = models.CharField(max_length=30) 
     last_name = models.CharField(max_length=30)
     age = models.IntegerField(default=0)
+    person = models.ForeignKey("Person", on_delete=models.CASCADE) #외래키
+    #지워졌을경우
+    #(CASCADE=같이지워짐)  (SET_NULL=null로 바꿈 null=True) (SET_DEFALT DEFALT로 바꿈 defalt='')
 
-    기존고객 = models.BooleanField(default=False)
-    유입경로 = models.CharField(choices=경로_선택,max_length=200)
-    인물사진 = models.ImageField(blank=True,null=True) #사진
-    파일 = models.FileField(blank=True,null=True) #blank=빈채로저장허용 null=db null
+    # 기존고객 = models.BooleanField(default=False)
+    # 인물사진 = models.ImageField(blank=True,null=True) #사진
+    # 파일 = models.FileField(blank=True,null=True) #blank=빈채로저장허용 null=db null
+
+
+class Person(models.Model):
+    회원 = models.OneToOneField(아이디, on_delete=models.CASCADE) #1대1
+
+
+    #python manage.py migrate
+
+    
